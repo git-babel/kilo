f282a8ba495cde6d8e2bf8c524b8b7e3b7df681b
Junyoung Lim <tlstjstksth@gmail.com>
2018-12-07 17:52:13 +0900

    Add error handlings

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,12 +7,21 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)) {
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios)) {
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -22,7 +32,9 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)) {
+        die("tcsetattr");
+    }
 }
 
 int main() {
@@ -30,7 +42,7 @@ int main() {
 
     while(1) {
         char c;
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
